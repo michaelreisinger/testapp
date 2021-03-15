61d29cf81e94b9f46ba15b3a653a1f8827206f1e
fhs43050 <mreisinger.itsb-b2018@fh-salzburg.ac.at>
2021-03-15 09:01:05 +0100

    first commit

---
+++ todolistserver/src/main/java/com/bactwo/todolistserver/TodolistserverApplication.java
@@ -0,0 +1,13 @@
+package com.bactwo.todolistserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodolistserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodolistserverApplication.class, args);
+	}
+
+}
