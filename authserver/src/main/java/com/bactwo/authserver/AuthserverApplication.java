6dec91cbca5d3c44291b0a4a2fad560cdbf4f2f7
fhs43050 <mreisinger.itsb-b2018@fh-salzburg.ac.at>
2021-03-18 08:45:53 +0100

    Auth Server setup

---
+++ authserver/src/main/java/com/bactwo/authserver/AuthserverApplication.java
@@ -0,0 +1,13 @@
+package com.bactwo.authserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthserverApplication.class, args);
+	}
+
+}
