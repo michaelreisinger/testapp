355f70be502da546b8c5302690f42cb5318b63a1
fhs43050 <mreisinger.itsb-b2018@fh-salzburg.ac.at>
2021-04-06 17:13:46 +0200

    basic swagger

--- authserver/src/main/java/com/bactwo/authserver/AuthserverApplication.java
+++ authserver/src/main/java/com/bactwo/authserver/AuthserverApplication.java
@@ -3,14 +3,30 @@ package com.bactwo.authserver;
 import com.bactwo.authserver.repository.UserRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackageClasses = UserRepository.class)
+@EnableSwagger2
 public class AuthserverApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AuthserverApplication.class, args);
+	}
+
+	@Bean
+	public Docket SwaggerConfig () {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.paths(PathSelectors.ant("/todo-api/v1/**"))
+				.apis(RequestHandlerSelectors.basePackage("com.bactwo"))
+				.build();
 	}
 
 }
--- todolistserver/src/main/java/com/bactwo/todolistserver/TodolistserverApplication.java
+++ todolistserver/src/main/java/com/bactwo/todolistserver/TodolistserverApplication.java
@@ -2,12 +2,28 @@ package com.bactwo.todolistserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class TodolistserverApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TodolistserverApplication.class, args);
+	}
+
+	@Bean
+	public Docket SwaggerConfig () {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.paths(PathSelectors.ant("/resource/v1/todolist/**"))
+				.apis(RequestHandlerSelectors.basePackage("com.bactwo"))
+				.build();
 	}
 
 }
